bcd8bd733f34d6b51d6c996a52c6ca7a9b217636
WillSumerfield <willsumerfield@gmail.com>
2023-11-10 02:19:49 -0800

    Functionalized
    Made most code into classes/functions

--- GameDemo.py
+++ GameDemo.py
@@ -3,21 +3,76 @@ import pygame
 import sys
 import math
 
-# Initialize Pygame
-pygame.init()
 
-# Constants
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
-BACKGROUND_COLOR = (135, 206, 235)  # Light Blue
-GRAVITY = 0.001
+# ---------- Function Definitions ----------
 
-# Initialize the screen
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Simple Platformer Game")
+# Do general initialization stuff here
+def initialize(screen_width, screen_height, ):
 
-# Player properties
+    # Initialize Pygame
+    pygame.init()
+
+    # Initialize the screen
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Simple Platformer Game")
+
+    return screen
+
+# Creates a new platform
+def generate_platform(x_offset):
+    return Platform(900 + x_offset, SCREEN_HEIGHT - 50, 100, 20)
+
+# Draw the player, the platforms, and the background to the screen. If you add new things to your game, update this function so that you can see them!
+def draw(screen, platforms, player):
+
+    # Draw the background
+    screen.fill(BACKGROUND_COLOR)
+
+    # Draw the platforms
+    for platform in platforms:
+        pygame.draw.rect(surface=screen, color=(0, 0, 0), rect=(platform.x, platform.y, platform.width, platform.height))
+
+    # Draw the player
+    pygame.draw.rect(surface=screen, color=(255, 0, 0), rect=(player.x, player.y, player.width, player.height))
+
+    # Update the screen
+    pygame.display.flip()
+
+# Check if the exit button is pressed - stop the game if so
+def exit_pressed():
+
+    # "Events" in pygame are things like mouse clicks, button clicks, keypresses, etc...
+    for event in pygame.event.get():
+
+        # If one of the events was pressing the "Exit" button, quit the game!
+        if event.type == pygame.QUIT:
+
+            # Return "True" to show that we should close the game
+            return True 
+        
+    # Return "False" to show that we should keep the game running
+    return False 
+
+# If a platforms goes off the screen, remove it!
+def remove_offscreen_platforms(platforms):
+
+    # How far a platform is from the left side of the screen
+    platform_dist = platforms[0].x + platforms[0].width 
+
+    # If the platform is fully offscreen, delete it!
+    if platform_dist < 0: 
+        del platforms[0] # Delete the platform
+        platforms += [generate_platform(platform_dist)] # Generate a new platform!
+
+    return platforms
+
+
+# ------------ Class Definitions ------------
+
+# The player
 class Player:
 
+    # Create a new player with the given properties
     def __init__(self, width, height, velx, vely, x, y, jump_power):
         self.width = width
         self.height = height
@@ -64,17 +119,89 @@ class Player:
         # If no platforms are below the player, return false
         return False
 
-player = Player(50, 50, 0.15, 0, 100, SCREEN_HEIGHT-120, -0.5)
+    # Move the player based on their speed
+    def move(self, platforms, delta_time):
 
-# Platform properties
+        # Apply gravity
+        player.vely = player.vely+(GRAVITY*delta_time)
+
+        # Move the player vertically
+        y_dist = self.vely*delta_time # The total vertical movement to move the player
+        if not self.platform_collision(platforms, 0, y_dist): # Try moving the player veritically. Does it collide with a platform?
+            self.y += y_dist # If it didn't collide, great! Move the player vertically.
+
+        # The player did collide with a platform when we tried moving it - move it one space at a time until it collides with the platform.
+        else: 
+            while not self.platform_collision(platforms, 0, math.copysign(1, y_dist)): # Keep moving the player until they collide with the platform
+                self.y += math.copysign(1, y_dist)
+
+        # Move the player horizontally (but actually move each of the platforms instead, to make it 'look' like the player is moving
+        x_dist = self.velx*delta_time # The total horizontal movement to move the player
+        if not self.platform_collision(platforms, x_dist, 0): # Try moving the player horizontally. Does it collide with a platform?
+
+            # If it didn't collide, great! Move each platform horizontally
+            for platform in platforms:
+                platform.x -= x_dist 
+        
+        # The player did collide with a platform when we tried moving it - move it one space at a time until it collides with the platform.
+        else: 
+            while not self.platform_collision(platforms, math.copysign(1, x_dist), 0): # Keep moving the player until they collide with the platform
+                for platform in platforms:
+                    platform.x -= math.copysign(1, x_dist)
+
+    # Returns True/False if the player is on the ground or not
+    def is_grounded(self):
+
+        # Check if there is a platform below the player
+        if player.platform_below(platforms): 
+
+            # If they're on the ground, they shouldn't be falling
+            player.vely = 0 
+
+            # The player is on the ground
+            return True
+        
+        # The player is not on the ground
+        return False
+
+# The platforms
 class Platform:
     
+    # Create a new platform with the given properties
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
 
+# Keeps track of the time which passed between each frame of the game, so we know how much to move things!
+class Clock:
+
+    # Create a new clock
+    def __init__(self):
+        self.previous_time = pygame.time.get_ticks() # The time it was last frame
+        self.current_time = pygame.time.get_ticks() # The current time
+
+    # Returns the time since the last frame
+    def get_delta_time(self):
+        current_time = pygame.time.get_ticks() # Find the current time
+        delta_time = current_time - self.previous_time # Find the time that has passed since the last frame and this one
+        self.previous_time = current_time # Update the previous time
+        return delta_time
+
+
+# Set up constant values here. Change they as you'd like!
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
+BACKGROUND_COLOR = (135, 206, 235)  # Light Blue
+GRAVITY = 0.001
+
+# Initialize the game, and return the screen we draw to
+screen = initialize(screen_width=SCREEN_WIDTH, screen_height=SCREEN_HEIGHT)
+
+# Create the player - jump power is negative because in pygame, negative y values are "up"!
+player = Player(width=50, height=50, velx=0.15, vely=0, x=100, y=SCREEN_HEIGHT-120, jump_power=-0.5)
+
+# Create the starting platforms - we will delete these and generate more as these go offscreen
 platforms = [
     Platform(150,   SCREEN_HEIGHT -  50, 100, 20),
     Platform(350,   SCREEN_HEIGHT -  50, 100, 20),
@@ -83,85 +210,39 @@ platforms = [
     Platform(950,   SCREEN_HEIGHT -  50, 100, 20),  
 ]
 
-# Creates a new platform
-def generate_platform(x_offset):
-    return Platform(900 + x_offset, SCREEN_HEIGHT - 50, 100, 20)
+# Create the clock timer
+clock = Clock()
 
 # Game loop
-running = True
-previous_time = pygame.time.get_ticks()
+running = True # If something sets this to "False", we exit the while loop and the program exits - effectively closing the game!
 while running:
 
-    # Find the time that has passed since the last frame
-    current_time = pygame.time.get_ticks()
-    delta_time = current_time - previous_time
-    previous_time = current_time
+    # Find how much time has passed since the last frame
+    delta_time = clock.get_delta_time()
 
-    # Check if we want to quit the game
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # Exit the game if we press the exit button
+    running = not exit_pressed()
 
     # Remove platforms which go off the screen, and generate new platforms as needed
-    platform_dist = platforms[0].x + platforms[0].width
-    if platform_dist < 0:
-        del platforms[0]
-        platforms += [generate_platform(platform_dist)]
+    platforms = remove_offscreen_platforms(platforms=platforms)
 
-    # Check if the player is grounded
-    grounded = False
-    if player.platform_below(platforms):
-        grounded = True
-        player.vely = 0
+    # Keeps track of if the player is on the ground or not
+    grounded = player.is_grounded()
 
     # Check for keypresses
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE] and grounded:
-       player.vely = player.jump_power
+    if keys[pygame.K_SPACE] and grounded: # If the player is on the ground and presses space, let them jump
+       player.vely = player.jump_power # Increase the player's y velocity by their jump power
 
-    # Apply gravity
-    player.vely = player.vely+(GRAVITY*delta_time)
-
-    # Move the player vertically
-    y_dist = player.vely*delta_time # The total vertical movement to move the player
-    if not player.platform_collision(platforms, 0, y_dist): # Try moving the player veritically. Does it collide with a platform?
-        player.y += y_dist # If it didn't collide, great! Move the player vertically.
-
-    # The player did collide with a platform when we tried moving it - move it one space at a time until it collides with the platform.
-    else: 
-        while not player.platform_collision(platforms, 0, math.copysign(1, y_dist)): # Keep moving the player until they collide with the platform
-            player.y += math.copysign(1, y_dist)
-
-    # Move the player horizontally (but actually move each of the platforms instead, to make it 'look' like the player is moving
-    x_dist = player.velx*delta_time # The total horizontal movement to move the player
-    if not player.platform_collision(platforms, x_dist, 0): # Try moving the player horizontally. Does it collide with a platform?
-
-        # If it didn't collide, great! Move each platform horizontally
-        for platform in platforms:
-            platform.x -= x_dist 
-    
-    # The player did collide with a platform when we tried moving it - move it one space at a time until it collides with the platform.
-    else: 
-        while not player.platform_collision(platforms, math.copysign(1, x_dist), 0): # Keep moving the player until they collide with the platform
-            for platform in platforms:
-                platform.x -= math.copysign(1, x_dist)
+    # Move the player as needed
+    player.move(platforms=platforms, delta_time=delta_time)
 
     # If the player fell off the map, quit the game
     if (player.y > SCREEN_HEIGHT):
         running = False
 
-    # Draw the background
-    screen.fill(BACKGROUND_COLOR)
-
-    # Draw the platforms
-    for platform in platforms:
-        pygame.draw.rect(screen, (0, 0, 0), (platform.x, platform.y, platform.width, platform.height))
-
-    # Draw the player
-    pygame.draw.rect(screen, (255, 0, 0), (player.x, player.y, player.width, player.height))
-
-    # Invert the screen
-    pygame.display.flip()
+    # Draw the player, the platforms, and the background to the screen. If you add new things to your game, update this function so that you can see them!
+    draw(screen=screen, platforms=platforms, player=player)
 
 # Clean up and quit
 pygame.quit()
